dc3e69d1e869ac43ef0ab6fb8ae976a2dbfca750
travis-scholtens <113060760+travis-scholtens@users.noreply.github.com>
2022-09-08 10:22:49 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,47 @@
+const rosters = {
+  'Division 1': {
+    'Some Team': [{
+        name: 'Susann Odell'
+      }
+    ]
+  },
+  'Division 2': {
+    'Another Team': [{
+        name: 'Caelinus Zeman',
+        rank: 1
+      },{
+        name: 'Melánia Ormond',
+        rank: 2
+      },{
+        name: 'Yehosheva Mertens',
+        rank: 3
+      },{
+        name: 'Wina Welch',
+        rank: 4
+      },{
+        name: 'Liam Kurucz',
+        rank: 5
+      },{
+        name: 'Margarid Yancy',
+      },{
+        name: 'Bipin Konishi',
+      },{
+        name: 'Euphemios Ngo',
+      },{
+        name: 'Albinus Rogerson',
+      },{
+        name: 'Maldwyn Peters',
+      },{
+        name: 'Ekber Daalman',
+      },{
+        name: 'Mose Hawk',
+      }
+    ]
+  },
+  'Division 3': {
+    'Some Team': [{
+        name: 'Presley Gronchi'
+      }
+    ]
+  }
+}
